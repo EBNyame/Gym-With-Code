c6b8806f5f95ef4bcb56544491c43463563cc7ef
ebnyame <ebnyame001@st.ug.edu.gh>
2024-11-30 21:53:54 +0000

    challenge6

---
+++ Challenge6.py
@@ -0,0 +1,6 @@
+# Write a Python program to convert kilometers to miles.
+
+kilometer = float(input("Enter your kilometer(s): "))
+# 1 kilometer = 0.621371 miles
+miles = kilometer * 0.621371
+print(f"{kilometer} kilometer(s) is equal to {miles} miles")
