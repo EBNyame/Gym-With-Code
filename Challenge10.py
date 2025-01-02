45a7839a02ff7c79b33706bd65e53ad841ed5b44
ebnyame <ebnyame001@st.ug.edu.gh>
2025-01-02 11:02:42 +0000

    anagram

---
+++ Challenge10.py
@@ -0,0 +1,9 @@
+def is_anagram(s1, s2):
+    if sorted(s1) == sorted(s2):
+        return True
+    else:
+        return False
+    
+
+print(is_anagram('listen', 'silent'))
+print(is_anagram('hello', 'world')) 
