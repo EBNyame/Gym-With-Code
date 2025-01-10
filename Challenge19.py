f5df6c2817643a72a3f1b5037722c2a22a732db0
ebnyame <ebnyame001@st.ug.edu.gh>
2025-01-10 06:20:39 +0000

    merging strings

---
+++ Challenge19.py
@@ -0,0 +1,13 @@
+# 
+def merge(w1, w2):
+    m = ""
+    minLen = min(len(w1), len(w2))
+    for i in range(minLen):
+        m += w1[i]
+        m += w2[i]
+    m += w1[minLen:]
+    m += w2[minLen:]
+    return m
+
+
+print(merge('Hello', 'World'))
