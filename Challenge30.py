ed6666e213348a32ec1bc4c1f1aeb9e01f405b28
ebnyame <ebnyame001@st.ug.edu.gh>
2025-04-24 16:28:39 +0000

    unpacking tuples

---
+++ Challenge30.py
@@ -0,0 +1,11 @@
+#  Given a tuple:
+info = ("Alice", 25, "Kenya")
+
+# Unpack it into name, age, and country variables.
+# Print each one on a separate line.
+
+name, age, country = info
+
+print(name)
+print(age)
+print(country)
